e77d2ca9e950d0e0a616a30f7065089d30033cb8
Volodymyr Viter <mad.tvido@gmail.com>
2021-03-26 16:31:58 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -19,18 +19,32 @@ refs.menu.insertAdjacentHTML('beforeend', cardsMarkup);
 refs.switch.addEventListener('change', setClassList);
 refs.switch.addEventListener('change', setLocalStorage);
 
-function setClassList(evt) {
+// function setClassList(evt) {
+//   const check = refs.switch.checked;
+//   if (check) {
+//     refs.body.classList.add(Theme.DARK);
+//     refs.body.classList.remove(Theme.LIGHT);
+//   } else {
+//     refs.body.classList.add(Theme.LIGHT);
+//     refs.body.classList.remove(Theme.DARK);
+//   }
+// }
+
+function switchTheme(x, y) {
+	refs.body.classList.add(x);
+	refs.body.classList.remove(y);
+}
+
+function setClassList() {
   const check = refs.switch.checked;
   if (check) {
-    refs.body.classList.add(Theme.DARK);
-    refs.body.classList.remove(Theme.LIGHT);
+    return switchTheme(Theme.DARK, Theme.LIGHT)
   } else {
-    refs.body.classList.add(Theme.LIGHT);
-    refs.body.classList.remove(Theme.DARK);
+    switchTheme(Theme.LIGHT, Theme.DARK)
   }
 }
 
-function setLocalStorage(evt) {
+function setLocalStorage() {
   const check = refs.switch.checked;
   if (check) {
     localStorage.setItem('theme', Theme.DARK);
